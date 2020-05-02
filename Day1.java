6e604dde5341f55a13b986bb21398e18865e08d3
Atul Pandey <atulpan98@gmail.com>
2020-05-02 20:37:06 +0530

    Added first question

---
+++ Day1.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        int mid = (high - low) / 2 + low;
+        while(low < high) {
+            if(isBadVersion(mid)) {
+                high = mid;
+            }   
+            else {
+                low = mid + 1;
+            }
+            mid = (high - low) / 2 + low;
+        }
+        return high;
+        
+    }
+}
