17de5f62ae3ee99ceb4d06b559cde30115066303
Atul Pandey <57017556+atulpandey98@users.noreply.github.com>
2020-05-12 15:37:52 +0530

    added new solution

---
+++ Week2/Day5.java
@@ -0,0 +1,13 @@
+// Single element in a sorted array
+// Use the property of XOR, A ^ A = 0.
+// Time complexity - O(n)
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int answer = 0;
+        for(int i : nums) {
+            answer = answer ^ i;
+        }
+        return answer;
+    }
+}
