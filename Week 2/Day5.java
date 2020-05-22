3f3a96832b7ceed7e7f0d2a6308b3f7a0db49b95
Atul Pandey <57017556+atulpandey98@users.noreply.github.com>
2020-05-22 12:04:28 +0530

    Single element in a sorted array

--- Week 2/Day5.java
+++ Week 2/Day5.java
@@ -1,6 +1,6 @@
 // Single element in a sorted array
 // Use the property of XOR, A ^ A = 0.
-// Time complexity - O(n)
+// Time complexity - O(n) 
 
 class Solution {
     public int singleNonDuplicate(int[] nums) {
